282173fa70825fa9aca0ba12009cacd8175055c5
Måns Rolandi <mans@phenixid.se>
2020-07-20 16:55:28 +0200

    Refactoring

--- StateMachine.js
+++ StateMachine.js
@@ -5,6 +5,23 @@ module.exports = function(nodes, preprocessors, state) {
 
     var current;
 
+    const next = function () {
+        var node = current[current.length - 1];
+        if (node.next === undefined) {
+            return Promise.resolve();
+        }
+        const nextId = node.next(state);
+        if (nextId === undefined) {
+            return Promise.resolve();
+        }
+        const next = nodes[nextId];
+        if (next === undefined) {
+            return Promise.reject(`[${node.id}] No next: ${nextId}`);
+        }
+        current = [next];
+        return Promise.resolve();
+    };
+
     return {
 
         setRoot(nodeId) {
@@ -12,11 +29,7 @@ module.exports = function(nodes, preprocessors, state) {
                 throw `Unknown node: ${nodeId}`;
             }
             current = [nodes[nodeId]];
-            return this.next();
-        },
-
-        output: function () {
-            return current.filter(node => node.output).map(node => node.output(state));
+            return next();
         },
 
         hasNext: function() {
@@ -24,36 +37,23 @@ module.exports = function(nodes, preprocessors, state) {
             return node.next !== undefined;
         },
 
-        next: function () {
-            var node = current[current.length - 1];
-            if (node.next === undefined) {
-                return Promise.resolve();
-            }
-            const nextId = node.next(state);
-            if (nextId === undefined) {
-                return Promise.resolve();
-            }
-            const next = nodes[nextId];
-            if (next === undefined) {
-                return Promise.reject(`[${node.id}] No next: ${nextId}`);
-            }
-            current = [next];
-            return Promise.resolve();
+        output: function () {
+            return current.filter(node => node.output).map(node => node.output(state));
         },
 
         process: function (input) {
             var node = current[current.length - 1]
             if (node.process === undefined) {
-                return this.next();
+                return next();
             }
             const preprocessor = preprocessors[node.type];
             if (!preprocessor) {
                 return Promise.resolve(node.process(state, input))
-                    .then(v => this.next());
+                    .then(v => next());
             }
             return Promise.resolve(preprocessor(state, input))
                 .then(processedInput => Promise.resolve(node.process(state, processedInput)))
-                .then(v => this.next());
+                .then(v => next());
         }
     }
 };
